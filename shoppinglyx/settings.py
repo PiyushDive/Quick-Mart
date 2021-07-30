b20727dc865f3045db4d57c2671f8f0a23394e44
Piyush Dive <piyushdive835@gmail.com.com>
2021-07-30 19:41:00 +0530

    static files

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -17,7 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
